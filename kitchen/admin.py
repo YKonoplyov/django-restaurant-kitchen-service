fa1a654c6c708285dab7329fdbd08327895d3404
Yaroslav Konoplyov <kannabis252@gmail.com>
2023-07-24 23:38:09 +0300

    admin page added

--- kitchen/admin.py
+++ kitchen/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import Group
 
 from kitchen.models import Cook, DishType, Dish, Position, Ingredient
+
+
+admin.site.unregister(Group)
 
 
 @admin.register(Cook)
